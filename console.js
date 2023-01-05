e61df9500f1d60aa61290ef5264537b1a379a26a
Chawput Nawakalanyu <chawput@gmail.com>
2023-01-05 16:46:16 +0700

    Update console.js

--- console.js
+++ console.js
@@ -1,23 +1,21 @@
 // เลือกใช้อย่างใดอย่างหนึ่ง ตามภาษาที่ใช้บน IG
 // IG ภาษาไทย
-setInterval(function() {
-  const followingButton = document.querySelector('button:contains("'กำลังติดตาม').")');
-  if (followingButton) {
-    followingButton.click();
-    setTimeout(function() {
-      document.querySelector('button:contains("'เลิกติดตาม').")').click();
-    }, 2000);
-  }
-}, 2000);
+setInterval(function(){
+	Array.from(document.querySelectorAll('button'))
+	 .find(el => el.textContent === 'กำลังติดตาม').click();
+	setTimeout(function(){
+		Array.from(document.querySelectorAll('button'))
+		.find(el => el.textContent === 'เลิกติดตาม').click();
+	},2000)
+},2000)
 
 
 // IG English Language
-setInterval(function() {
-  const followingButton = document.querySelector('button:contains("Following")');
-  if (followingButton) {
-    followingButton.click();
-    setTimeout(function() {
-      document.querySelector('button:contains("Unfollow")').click();
-    }, 2000);
-  }
-}, 2000);
+setInterval(function(){
+	Array.from(document.querySelectorAll('button'))
+	 .find(el => el.textContent === 'Following').click();
+	setTimeout(function(){
+		Array.from(document.querySelectorAll('button'))
+		.find(el => el.textContent === 'Unfollow').click();
+	},2000)
+},2000)
